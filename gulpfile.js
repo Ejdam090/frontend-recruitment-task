31a688732afd293730dbde22fcaac442569a8bb3
AdamKosior <a.kosior090@wp.pl>
2022-06-06 22:56:14 +0200

    added media queries, corected html file

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,68 @@
-const { src, dest, watch, series, parallel, gulp} = require('gulp');
+const { src, dest, watch, series, parallel, gulp } = require("gulp");
 
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const browesersync = require('browser-sync').create();
-var replace = require('gulp-replace');
-const gulpConcat = require('gulp-concat');
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const browesersync = require("browser-sync").create();
+var replace = require("gulp-replace");
+const gulpConcat = require("gulp-concat");
 
 // File paths
-const files = { 
-    scssPath: 'src/scss/**/*.scss',
-    jsPath: 'src/js/**/*.js'
+const files = {
+  scssPath: "src/scss/**/*.scss",
+  jsPath: "src/js/**/*.js",
 };
 
-function scssTask(){    
-    return src(files.scssPath)
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass([])) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest('dist')
-    ); // put final CSS in dist folder
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init()) // initialize sourcemaps first
+    .pipe(sass([])) // compile SCSS to CSS
+    .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+    .pipe(sourcemaps.write(".")) // write sourcemaps file in current directory
+    .pipe(dest("dist")); // put final CSS in dist folder
 }
 
-
-function jsTask(){
-    return src([
-        files.jsPath
-        ])
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(dest('dist')
-    );
+function jsTask() {
+  return src([files.jsPath])
+    .pipe(concat("all.js"))
+    .pipe(uglify())
+    .pipe(dest("dist"));
 }
 
 var cbString = new Date().getTime();
-function cacheBustTask(){
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
+function cacheBustTask() {
+  return src(["index.html"])
+    .pipe(replace(/cb=\d+/g, "cb=" + cbString))
+    .pipe(dest("."));
 }
 
-function watchTask(){
-    
-    watch([files.scssPath, files.jsPath], 
-        parallel(scssTask, jsTask));   
-        watch('*.html', browsersyncReload); 
-        watch('src/scss/**/*.scss', browsersyncReload);
-        watch( 'src/js/*.js', browsersyncReload);
+function watchTask() {
+  watch([files.scssPath, files.jsPath], parallel(scssTask, jsTask));
+  watch("*.html", browsersyncReload);
+  watch("src/scss/**/*.scss", browsersyncReload);
+  watch("src/js/*.js", browsersyncReload);
 }
-function browsersyncServe(cb){
-    browesersync.init({
-        server:{
-            baseDir:'.'
-        }
-    });
-    cb();
+function browsersyncServe(cb) {
+  browesersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
 }
 
-function browsersyncReload(cb){
-    browesersync.reload();
-    cb();
+function browsersyncReload(cb) {
+  browesersync.reload();
+  cb();
 }
-
 
 exports.default = series(
-    parallel(scssTask, jsTask), 
-    cacheBustTask,
-    browsersyncServe,
-    watchTask
+  parallel(scssTask, jsTask),
+  cacheBustTask,
+  browsersyncServe,
+  watchTask
 );
-
