b4e3a314ab77cfba34870e121ed90318640a700d
AdamKosior <a.kosior090@wp.pl>
2022-06-06 14:52:20 +0200

    orrected js file, corrected html file

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,37 @@
 /// variabels
-const btn = document.querySelector(".btn");
-const popupText = document.querySelector(".popup-text");
-const popupShadow = document.querySelector('.popup-shadow');
-const popupContainer =document.querySelector('.popup-container');
-const xmark = document.querySelector('#close');
-const resetBTn = document.querySelector(".reset"); 
+const btn = document.querySelector(".content-btn");
+const popupCounter = document.querySelector(".popup-counter");
+const popupShadow = document.querySelector(".popup-shadow");
+const xmark = document.querySelector("#close");
+const resetBTn = document.querySelector(".popup-reset");
 let count = localStorage.getItem("count") || 0;
 
 /// function which show popup
-const showPanel =()=>{
-   if(!(popupShadow.style.display === "block")){
-       popupShadow.style.display = "block";
-       count ++;
-   }
-   else{
-       popupShadow.style.display = "none";
-   }
-   if(count > 4){
-       resetBTn.style.display = "block";
-       resetBTn.addEventListener("click", ()=>{
-        count = 0;
-    })
-   }
-   localStorage.setItem("count",count);
-   popupText.innerHTML = `You have clicked <strong>${count} times</strong> to related button`;
-} 
+const showPanel = () => {
+  if (!(popupShadow.style.display === "block")) {
+    popupShadow.style.display = "block";
+    count++;
+    popupCounter.textContent = `${count} times`;
+  } else {
+    popupShadow.style.display = "none";
+  }
 
-
+  if (count > 4) {
+    resetBTn.style.display = "block";
+  }
+  localStorage.setItem("count", count);
+};
 
 //listener on click
 
 btn.addEventListener("click", showPanel);
-xmark.addEventListener("click",showPanel);
-window.addEventListener("click", (e)=>{
-    e.target === popupShadow ? showPanel() : false
+xmark.addEventListener("click", showPanel);
+window.addEventListener("click", (e) => {
+  e.target === popupShadow ? showPanel() : false;
+});
+resetBTn.addEventListener("click", () => {
+  count = 0;
+  localStorage.setItem("count", count);
+  popupCounter.textContent = `${count} times`;
+  resetBTn.style.display = "none";
 });
